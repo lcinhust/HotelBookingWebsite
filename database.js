b73605af94b0d7a178f2553804420be190503dae
Le Xuan Hieu <lexuanhieu20032003@gmail.com>
2023-02-02 16:16:32 +0700

    Signin Signup

---
+++ database.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql');
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'quenmkroi123', /// thay doi password
+    database: 'bookingapp' // ghi ten database cua minh vao
+  });
+
+db.connect((err)=>{
+  if (!err) 
+      console.log('Connect successfully');
+  else console.log(err);
+})
+
+module.exports=db;
--- routes/general.js
+++ routes/general.js
@@ -2,6 +2,19 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
+function checkAuthLogin(req,res,next){
+    if (req.session.userId)
+        res.redirect('/booking');
+    else next();
+}
+
+function checkAuthProtectedRoute(req,res,next)
+{
+    if (req.session.userId)
+        next();
+    else res.redirect('/loginform');
+}
+
 router.get('/index', (req, res) => {
     res.sendFile('index.html', { root: path.join(`${__dirname}/../pages`)});
 })
@@ -14,17 +27,17 @@ router.get('/about', (req, res) => {
     res.sendFile('about.html', { root: path.join(`${__dirname}/../pages`)});
 })
 
-router.get('/booking', (req, res) => {
+router.get('/booking', checkAuthProtectedRoute, (req, res) => {
     res.sendFile('booking.html', { root: path.join(`${__dirname}/../pages`)});
-})
+}) //must log in to see
 
 router.get('/contact', (req, res) => {
     res.sendFile('contact.html', { root: path.join(`${__dirname}/../pages`)});
 })
 
-router.get('/loginform', (req, res) => {
+router.get('/loginform', checkAuthLogin, (req, res) => {
     res.sendFile('loginform.html', { root: path.join(`${__dirname}/../pages`)});
-})
+})//if already logged in, redirect to /booking
 
 router.get('/restaurant', (req, res) => {
     res.sendFile('restaurant.html', { root: path.join(`${__dirname}/../pages`)});
