e1cbf776b56faf7b7a8c6eafff654cafaab57990
cokhibk <lcnguyeninhust@gmail.com>
2023-02-09 10:01:50 +0700

    Add from LXH branch

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+require('dotenv').config();
+const session=require('express-session');
+
+const bodyParser = require('body-parser');
+const usersRouter = require('./routes/users');
+const generalRouter = require('./routes/general');
+
+
+const app = express();
+
+const oneDay = 1000 * 60 * 60 * 24;
+app.use(session({
+    secret: "thisismysecretkey",
+    saveUninitialized:true,
+    cookie: { maxAge: oneDay },
+    resave: false,
+}));
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.json());
+
+app.use(generalRouter);
+app.use(usersRouter);
+
+
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
+})
